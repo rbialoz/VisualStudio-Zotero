f91ca00e84113b0cc23afb7e2f00a562a607160a
rbialoz <ronald.bialozyt@nw-fva.de>
2025-07-30 16:12:53 +0200

    Meta Viewort

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ $displayItems = filter_items($items, $search);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zotero Entries Viewer</title>
     <style>
         body { font-family: Arial, sans-serif; margin: 2em; }
